0ebb4ff709d8af4ab1c2f9cc6de9ee1874ffb692
omarurregodev <omarurrego07@gmail.com>
2022-10-18 09:22:32 -0500

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,51 @@
+const socket = io.connect();
+
+socket.on("productos", (data) => {
+    showProductos(data)
+});
+
+function showProductos(data) {
+    const htmlProductos = data.map((prod) => {
+        return `
+        <tr><td>${prod.nombre}</td><td>$ ${prod.precio}</td><td><img src="${prod.url}" width="50" alt="not found"></td></tr>
+        `
+    }).join(" ")
+    document.getElementById("tableBodyProd").innerHTML = htmlProductos;
+}
+function addProduct(e) {
+    const producto = {
+        nombre: document.getElementById("nombre").value,
+        precio: document.getElementById("precio").value,
+        url: document.getElementById("foto").value
+    };
+    socket.emit("new-product", producto);
+    return false;
+}
+
+socket.on("messages", (data) => {
+    showMessages(data);
+})
+
+function showMessages(data) {
+    const htmlMessages = data.map((msg) => {
+        return `
+        <div>
+            <strong style="color: blue;">${msg.author}</strong>
+            <em style="color: brown;">[${msg.date}] :</em>
+            <em style="color: green;">${msg.text}</em>
+        </div>
+        `
+    }).join(" ");
+
+    document.getElementById("chatZone").innerHTML = htmlMessages;
+}
+
+function addMessages(e) {
+    const mensaje = {
+        author: document.getElementById("email").value,
+        date: new Date().toLocaleString(),
+        text: document.getElementById("texto").value
+    }
+    socket.emit("new-message", mensaje);
+    return false;
+}
---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const PORT = 8000;
+const { Server: HttpServer } = require("http");
+const { Server: IOServer } = require("socket.io");
+
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
+
+app.use(express.static("./public"));
+app.get("/", (req, res) => {
+    res.sendFile("index.html", { root: __dirname })
+});
+
+const productos = [
+    {
+        nombre: "Primer prod",
+        precio: 234.54,
+        url: "www.google.co"
+    }
+];
+
+const messages = [
+    {
+        author: "Juan@gmail.com",
+        date: new Date().toLocaleString(),
+        text: "Hola que tal"
+    },
+    {
+        author: "Maria@gmail.com",
+        date: new Date().toLocaleString(),
+        text: "bien y tu"
+    },
+    {
+        author: "Juan@gmail.com",
+        date: new Date().toLocaleString(),
+        text: "Me alegra"
+    }
+];
+
+
+io.on("connection", (socket) => {
+    console.log("Se conectó un usuario!");
+
+    socket.emit("productos", productos);
+    
+    socket.on("new-product", (data) => {
+        productos.push(data);
+        io.sockets.emit("productos", productos)
+    })
+    
+    socket.emit("messages", messages);
+    
+    socket.on("new-message", (data) => {
+        messages.push(data);
+        io.sockets.emit("messages", messages)
+    })
+}) 
+
+const connectedServer = httpServer.listen(PORT, () => {
+    console.log("Server On")
+});    
